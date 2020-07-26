d5c76b9b86531ae3deeefbafdc1a9ce39bb76e46
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-07-26 21:56:51 +0800

    Updates

---
+++ precache-manifest.67fe4b9048414a954b7ce6d18e353a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106d8c928476238c3e2c9d4af33e18e1",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "e2129a611a18bb382a52",
+    "url": "/lottery/static/css/main.bac4da6c.chunk.css"
+  },
+  {
+    "revision": "11c5828b00aec5d843a2",
+    "url": "/lottery/static/js/2.5768adac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery/static/js/2.5768adac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2129a611a18bb382a52",
+    "url": "/lottery/static/js/main.d7a1b046.chunk.js"
+  },
+  {
+    "revision": "3c6c463699d790bdbf8a",
+    "url": "/lottery/static/js/runtime-main.616fde27.js"
+  }
+]);
